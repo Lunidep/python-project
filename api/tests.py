4ca3d93eac6a5aceb69ca259dc21de93c5aa016e
Lunidep <ilya151102@yandex.ru>
2025-01-04 18:42:12 +0300

    fix tests

--- api/tests.py
+++ api/tests.py
@@ -7,7 +7,7 @@ class PingPongTests(TestCase):
     def test_ping(self):
         response = self.client.get(reverse('ping'))
         self.assertEqual(response.status_code, 200)
-        self.assertJSONEqual(str(response.content, encoding='utf8'), {'message': 'pong'})
+        self.assertJSONEqual(str(response.content, encoding='utf8'), {'message': 'django lol'})
 
     def test_pong(self):
         response = self.client.get(reverse('pong'))
