d4083c133671fa0ef1255c503e74d6bc83c3bab6
sanecheqq <semin.alex222@yandex.ru>
2025-01-07 14:27:50 +0300

    fix structure

--- api/management/commands/run_bot.py
+++ api/management/commands/run_bot.py
@@ -1,6 +1,6 @@
 import telebot
 
-API_TOKEN = 'AAF8dCBOS7WO2pzWSyXt6Zl8ZR-3FZ0R8Go'
+API_TOKEN = '7688662807:AAF8dCBOS7WO2pzWSyXt6Zl8ZR-3FZ0R8Go'
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
