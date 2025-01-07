c5bb95af0cdf03cf5dc6a5816a7ba1276fbdf8c8
sanecheqq <semin.alex222@yandex.ru>
2025-01-07 13:18:46 +0300

    bot init

---
+++ pingpong/managment/commands/run_bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+API_TOKEN = 'AAF8dCBOS7WO2pzWSyXt6Zl8ZR-3FZ0R8Go'
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я помогу вам найти идеальное место для отдыха. Укажите ваши предпочтения.")
+
+@bot.message_handler(func=lambda message: True)
+def recommend_place(message):
+    # Реализуйте логику анализа сообщений и рекомендации мест
+    bot.reply_to(message, "Рекомендуемое место: Парк Горького")
+
+bot.polling()
